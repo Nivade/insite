da4068c945d9ff6738a34e9a959b20cfbc803e11
Niels van Deursen <nhavandeursen@gmail.com>
2016-01-13 12:32:38 +0100

    PT1.2 added to repo

---
+++ Insite/PT1.2/PT1.2/Database.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+
+namespace PT1._2
+{
+    class Database
+    {
+        // De bestandsnaam voor de database
+        private static readonly string databaseFilename = "Database.sqlite";
+        // Variabele voor het opzetten van de verbinding
+        private static SQLiteConnection connection;
+        // Variable waar de SQL-commandos tijdelijk in opgeslagen worden
+        private static SQLiteCommand command;
+
+
+
+        public static string Query
+        {
+            set
+            {
+                // Zorg ervoor dat er een verbinding gemaakt kan worden
+                PrepareConnection();
+                // Stel het SQL commando in met de gegeven query
+                command = new SQLiteCommand(value, connection);
+            }
+        }
+
+        /// <summary>
+        /// Haalt het command-object op waarmee queries uitgevoerd kunnen worden.
+        /// </summary>
+        public static SQLiteCommand Command
+        {
+            get { return command; }
+        }
+
+        /// <summary>
+        /// Haal de bestandsnaam op van de database.
+        /// </summary>
+        public static string DatabaseFilename
+        {
+            get { return databaseFilename; }
+        }
+
+        /// <summary>
+        /// Open de verbinding met de database
+        /// </summary>
+        public static void OpenConnection()
+        {
+            // Controleer of de verbinding niet al open is
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+            }
+        }
+        public static void OpvragenRoute(string naam, string beginD, string beginTijd, string EindD, string eindtijd)
+        {
+            
+        }
+
+        public static void Save(string v, int x, int y, int width, int height)
+        {
+
+          
+            Database.Query = "INSERT INTO Locatie (LocatieID INT, KamerID INT, GebruikerID INT, DT datetime) values ('" + v + "'," + x + " ," + y + " ," + width + " ," +
+                               height + ")";
+
+            Database.OpenConnection();
+            Database.Command.ExecuteNonQuery();
+            Database.CloseConnection();
+        }
+
+        /// <summary>
+        /// Sluit de verbinding met de database
+        /// </summary>
+        public static void CloseConnection()
+        {
+            // Controleer of de verbinding niet al gesloten is
+            if (connection.State != System.Data.ConnectionState.Closed)
+            {
+                connection.Close();
+            }
+        }
+
+        private static void PrepareConnection()
+        {
+            // Controleer of we een nieuwe database met dummy data moeten aanmaken
+            bool createNew = !File.Exists("Database.sqlite");
+
+            // Bestand bestaat niet: maak een lege database aan
+            if (createNew)
+            {
+                SQLiteConnection.CreateFile(databaseFilename);
+            }
+
+            // Zet een verbinding op met de database
+            if (connection == null)
+            {
+                connection = new SQLiteConnection("Data Source=" + databaseFilename + ";Version=3");
+            }
+
+            // Als we een nieuwe database gemaakt hebben, voegen we alvast wat records toe.
+            // We doen dit nu pas omdat we een connection nodig hebben om te communiceren met
+            // de database: vandaar dat deze code niet boven bij de CreateFile functie staat.
+            if (createNew)
+            {
+                CreateDummyData();
+            }
+        }
+
+        /// <summary>
+        /// Functie die een nieuwe tabel aanmaakt op een lege database, en deze vult met een
+        /// aantal records.
+        /// </summary>
+        private static void CreateDummyData()
+        {
+            OpenConnection();
+
+            try
+            {
+                // Nieuwe tabel met drie kolommen: number (INT), name (VARCHAR), email (VARCHAR)
+                Query = "CREATE TABLE Locatie (LocatieID INT, KamerID INT, GebruikerID INT, DT datetime)";
+                Command.ExecuteNonQuery();
+
+                /* Voeg nieuwe rijen toe aan de database
+                 Query = "INSERT INTO vormen (vorm, x, y, width, height) values ('c', 0, 0, 0, 0)";
+                 Command.ExecuteNonQuery();
+                 Query = "INSERT INTO vormen (vorm, x, y, width, height) values ('r', 0, 0, 0, 0)";
+                 Command.ExecuteNonQuery();
+                 Query = "INSERT INTO vormen (vorm, x, y, width, height) values ('l', 0, 0, 0, 0)";
+                 Command.ExecuteNonQuery(); */
+            }
+            catch (SQLiteException)
+            {
+                // Er is iets mis gegaan: waarschijnlijk bestond de tabel al. Voor nu is er
+                // verder geen foutafhandeling nodig.
+            }
+
+            CloseConnection();
+        }
+
+       
+    }
+}
+
---
+++ Insite/PT1.2/PT1.2/Locatie.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace PT1._2
+{
+    class Locatie
+    {
+        private string _naam;
+        private int _gebruikersID;
+        private int _kamerID;
+        private string _beginDT;
+        private string _eindDT;
+        private string _datum;
+        private static int 
+
+
+        /// <summary>
+        /// Maakt een nieuwe instantie aan van een Student.
+        /// </summary>
+        /// <param name="number">Het nummer van de student.</param>
+        /// <param name="name">De naam van de student.</param>
+        /// <param name="email">Het email-adres van de student.</param>
+        public Locatie(string naamTmp, int gebruikersIDTmp, int kamerIDTmp, string DatumTmp)
+        {
+            this._naam = naamTmp;
+            this._gebruikersID= gebruikersIDTmp;
+            this._kamerID = kamerIDTmp;
+            this._datum = DatumTmp;
+
+
+        }
+
+        /// <summary>
+        /// Haalt alle studenten op die in de database staan, gesorteerd op studentnummer.
+        /// </summary>
+        /// <returns>Een lijst van studenten.</returns>
+        /// 
+        public string ReadOnlyVorm                 // Property
+        {
+            get { return _vorm; }         // Getter
+        }
+        public int ReadOnlyX                // Property
+        {
+            get { return _x; }         // Getter
+        }
+        public int ReadOnlyY               // Property
+        {
+            get { return _y; }         // Getter
+        }
+        public int ReadOnlyWidth                // Property
+        {
+            get { return _width; }         // Getter
+        }
+        public int ReadOnlyHeight                // Property
+        {
+            get { return _height; }         // Getter
+        }
+        public static List<Locatie> LoadAllFromDatabase(string naam, string beginD, string beginTijd, string EindD, string eindtijd)
+        {
+            int gebruikersID = OpvragenGebruikersID(naam);
+            int datum;
+
+            Database.Query = "SELECT * FROM GebruikerLocatie Where gebruikerID = " + Convert.ToString(gebruikersID)+ " and dt is between ";
+            Database.OpenConnection();
+
+            // De resultaten worden nu opgeslagen in een "reader": deze wordt in de while-loop
+            // verderop gebruikt om nieuwe instanties van studenten aan te maken
+            SQLiteDataReader reader = Database.Command.ExecuteReader();
+
+            // Onderstaande list bevat alle studenten die uitgelezen worden
+            List<Locatie> VormenLijst = new List<Locatie>();
+            while (reader.Read())
+            {
+                // Voeg een nieuwe student toe door de kolommen mee te geven aan de constructor
+                VormenLijst.Add(new Locatie(naam,
+                                         gebruikersID,
+                                         Convert.ToInt32(reader["KamerID"]),
+                                         Convert.ToString(reader["DT"])
+                                         ));
+            }
+
+            Database.CloseConnection();
+
+            // Retourneer de lijst met studenten
+            return VormenLijst;
+        }
+
+        public static int OpvragenGebruikersID(string naam)
+        {
+            int gID = 0;
+            Database.Query = "select * from Gebruiker where naam = " + naam;
+            Database.OpenConnection();
+            SQLiteDataReader reader = Database.Command.ExecuteReader();
+           // while (reader.Read())
+            //{
+                gID = Convert.ToInt32(reader["GebruikerLocatieID"]);
+           // }
+            return gID;
+        }
+        public static int WarmingPing(string naam)
+        {
+            int gID = 0;
+            Database.Query = "select * from Gebruiker where naam = " + naam;
+            Database.OpenConnection();
+            SQLiteDataReader reader = Database.Command.ExecuteReader();
+            // while (reader.Read())
+            //{
+            gID = Convert.ToInt32(reader["GebruikerLocatieID"]);
+            // }
+            return gID;
+        }
+
+        /// <summary>
+        /// Slaat een nieuwe student op in de database. Toont een messagebox als er al
+        /// een student met het gegeven nummer in de database staat.
+        /// </summary>
+        /// <param name="number">Het nummer van de student.</param>
+        /// <param name="name">De naam van de student.</param>
+        /// <param name="email">Het email-adres van de student.</param>
+        /// <returns>True als het toevoegen gelukt is; false als er een fout opgetreden is.</returns>
+    }
+}
+}
