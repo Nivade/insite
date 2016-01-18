41529e0ca456a3c8aeaed947406918752aef98cc
Mindavi <rol3517@gmail.com>
2016-01-18 11:09:03 +0100

    added project to scan for wifi mac addresses

---
+++ Insite/StrongestMacAddress/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NativeWifi;
+
+namespace StrongestMacAddress
+{
+    class Program
+    {
+        static string Data;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input ssid:");
+            string ssid = Console.ReadLine();
+            int length = ssid.Length;
+            for (int i = 0; i < (32 - length); i++)
+            {
+                ssid += '\0';
+            }
+            WlanClient client = new WlanClient();
+            begin:
+            WifiDetection(client, ssid);
+            System.Threading.Thread.Sleep(5000);
+            goto begin;
+        }
+
+        private static void WifiDetection(WlanClient client, string ssid)
+        {
+            foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
+            {
+
+                Wlan.WlanBssEntry[] wlanBssEntries = wlanIface.GetNetworkBssList();
+
+                foreach (Wlan.WlanBssEntry network in wlanBssEntries)
+                {
+                    int rss = network.rssi;
+                    byte[] macAddr = network.dot11Bssid;
+                    string tMac = "";
+
+                    for (int i = 0; i < macAddr.Length; i++)
+                    {
+
+                        tMac += macAddr[i].ToString("x2").PadLeft(2, '0').ToUpper();
+
+                    }
+                    string SSID = Encoding.ASCII.GetString(network.dot11Ssid.SSID).ToString();
+
+                    if (ssid == SSID)
+                    {
+
+                        Data += network.linkQuality;
+                        Data += ";";
+                        Data += tMac;
+                        Data += ";";
+                    }
+                }
+            }
+            Console.WriteLine("data: " + Data);
+            Data = null;
+        }
+    }
+}
